7bc4fdca3b59a89437faaf6fb9d661bdbfb5cc77
retroboydev <iberezkin90@gmail.com>
2022-11-11 10:36:33 +0100

    added nft feature

--- src/features/nft/NftCarousel/NftCarousel.jsx
+++ src/features/nft/NftCarousel/NftCarousel.jsx
@@ -1,92 +1,95 @@
 import { useTranslation } from 'react-i18next';
-import React, { useCallback } from 'react';
+import React, { useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FreeMode } from 'swiper';
-import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Skeleton from '@mui/material/Skeleton';
-import Link from '@mui/material/Link';
-import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
-import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
-import EmojiEventsOutlinedIcon from '@mui/icons-material/EmojiEventsOutlined';
+import { FreeMode } from 'swiper';
+import { useSelector } from 'react-redux';
 
 import 'swiper/css';
 import 'swiper/css/bundle';
 
-import Stack from '@mui/material/Stack';
-
 import { NftCarouselContainer, NftCarouselHeader, NftCarouselContent } from './NftCarousel.styles';
 import NftCard from '../NftCard/NftCard';
+import { useLazyGetMeshNftByUserIdQuery } from '../../api/apiSlice';
+import { selectProfileAddress } from '../../profile/profileSlice';
 
-const noop = () => {};
+const NftCarousel = () => {
+  const { t } = useTranslation();
+  const profileId = useSelector(selectProfileAddress);
 
-const NftCarousel = ({ }) => (
-  <NftCarouselContainer>
-    <NftCarouselHeader>
-      <Typography variant="h6" color="text.primary">Mesh NFTs</Typography>
-    </NftCarouselHeader>
-    <NftCarouselContent>
-      <Swiper
-        modules={[FreeMode]}
-        spaceBetween={20}
-        slidesPerView="auto"
-        // grabCursor
-        // freeMode
-        // width={200}
-        resistance
-        resistanceRatio={0}
-        // touchReleaseOnEdges
-      >
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
-        <SwiperSlide>
-          <NftCard />
-        </SwiperSlide>
+  const [getMeshNftByUserId, {
+    data: nfts = {},
+    isLoading,
+    isSuccess,
+    isError,
+    isUninitialized,
+  }] = useLazyGetMeshNftByUserIdQuery();
+
+  useEffect(() => {
+    if (!profileId) { return; }
+    getMeshNftByUserId(profileId);
+  }, [profileId]);
+
+  const isEmpty = !nfts?.ids?.length;
+
+  let content;
+  if (isLoading || isUninitialized) {
+    content = <MockNftCarousel />;
+  } else if (isError) {
+    content = <ErrorNftCarousel />;
+  } else if (isEmpty) {
+    content = <EmptyNftCarousel />;
+  } else if (isSuccess) {
+    content = (
+      <Swiper modules={[FreeMode]} spaceBetween={20} slidesPerView="auto" resistance resistanceRatio={0}>
+        {nfts.ids.map((id) => (
+          <SwiperSlide key={id}>
+            <NftCard {...nfts.entities[id]} />
+          </SwiperSlide>
+        ))}
       </Swiper>
-    </NftCarouselContent>
-  </NftCarouselContainer>
-);
+    );
+  }
+
+  return (
+    <NftCarouselContainer>
+      <NftCarouselHeader>
+        <Typography variant="h6" color="text.primary">{t('meshNftsCarousel.title')}</Typography>
+      </NftCarouselHeader>
+      <NftCarouselContent>
+        <Swiper modules={[FreeMode]} spaceBetween={20} slidesPerView="auto" resistance resistanceRatio={0}>
+          {content}
+        </Swiper>
+      </NftCarouselContent>
+    </NftCarouselContainer>
+  );
+};
+
+const MockNftCarousel = () => {
+  const data = Array(12).fill();
+  return (
+    <Swiper enabled={false} spaceBetween={20} slidesPerView="auto">
+      {data.map((_, key) => (
+        <SwiperSlide key={key}>
+          <NftCard isLoading />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+const ErrorNftCarousel = () => {
+  const { t } = useTranslation();
+  return (
+    <Typography variant="body1" color="text.primary">{t('meshNftsCarousel.errors.fetching')}</Typography>
+  );
+};
+const EmptyNftCarousel = () => {
+  const { t } = useTranslation();
+  return (
+    <Typography variant="body1" color="text.primary">{t('meshNftsCarousel.errors.empty')}</Typography>
+  );
+};
 
 NftCarousel.propTypes = {};
 
